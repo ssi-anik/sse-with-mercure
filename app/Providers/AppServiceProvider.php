02052ddb66d6679bed572b79117d516e8ba928bc
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-02-15 12:48:19 +0600

    installed symfony mercure

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Symfony\Component\Mercure\Jwt\StaticJwtProvider;
+use Symfony\Component\Mercure\Publisher;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(Publisher::class, function () {
+            return (new Publisher('http://mercure/.well-known/mercure', (new StaticJwtProvider(env('MERCURE_JWT_TOKEN')))));
+        });
     }
 
     /**
