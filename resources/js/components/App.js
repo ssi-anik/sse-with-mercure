9d7b5b2ac12ca79ac9c220df9d44d7ed0355e2d5
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-02-19 00:16:06 +0600

    broadcast message from frontend, show data to user added

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,24 +1,147 @@
-import React from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
+import {NativeEventSource, EventSourcePolyfill} from 'event-source-polyfill';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
-    return (
-        <div className="container mt-5">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card text-center">
-                        <div className="card-header"><h2>React Component in Laravel</h2></div>
+window.EventSourceOf = NativeEventSource || EventSourcePolyfill;
+window.EventSourcePolyfill = EventSourcePolyfill;
 
-                        <div className="card-body">I'm tiny React component in Laravel app!</div>
-                    </div>
+function App () {
+    const [topicRef, messageRef] = [
+        useRef(),
+        useRef(),
+    ];
+    const [messages, setMessage] = useState([]);
+    const [messageType, setMessageType] = useState('public');
+    const [hub, setHub] = useState('');
+
+    const addMessage = (message, type = 'info') => {
+        if ( typeof message != "string" ) {
+            message = message.message
+        }
+
+        setMessage(prev => [
+            {type, text: message},
+            ...prev
+        ]);
+    }
+
+    useEffect(() => {
+        fetch('/api/discovery').then(response => {
+            addMessage(`Hub discovery. Status: "${response.status}"`);
+            if ( response.ok ) {
+                return response;
+            }
+
+            return Promise.reject(response);
+        }).then(response => {
+            const hubUrl = response.headers.get('Link').match(/<([^>]+)>;\s+rel=(?:mercure|"[^"]*mercure[^"]*")/)[1];
+            setHub(() => hubUrl);
+            addMessage(`Found hub: "${hubUrl}"`, 'success')
+        }).catch(e => {
+            addMessage("Cannot find Hub URL.", "danger");
+            console.log(e);
+        });
+    }, []);
+
+    const publishEvent = () => {
+        const topic = topicRef.current.value.trim();
+        const message = messageRef.current.value.trim();
+        if ( !topic || !message ) {
+            alert('Topic Or Message cannot be empty');
+            return;
+        }
+        const payload = {
+            'topic': topicRef.current.value, 'message': messageRef.current.value, 'publish_type': messageType,
+        }
+
+        fetch('/api/broadcast', {
+            headers: {
+                'content-type': 'application/json',
+            }, method: 'post', body: JSON.stringify(payload)
+        }).then(response => {
+            addMessage(`Broadcast message. Status: "${response.status}"`);
+            if ( response.ok ) {
+                return response;
+            }
+
+            return Promise.reject(response);
+        }).then(() => {
+            addMessage('Successfully broadcasts message.', 'success');
+        }).catch(e => {
+            addMessage("Check console.", 'warning');
+            console.log(e);
+        });
+    }
+
+    const changeMessageType = (value) => {
+        setMessageType(prev => value)
+    }
+
+    return (<div className = "container-fluid" style = {{marginTop: 5}}>
+        <div className = "row">
+            <div className = "col-8">
+                <div className = "col-12">
+                    <p className = "text-info">Publish a message</p>
+                    <form className = "form-row" onSubmit = {e => e.preventDefault()}>
+                        <div className = "form-group col-4">
+                            <input type = "text" placeholder = "Topic" ref = {topicRef} name = "topic"
+                                   className = "form-control" />
+                        </div>
+                        <div className = "form-group col-4">
+                            <input type = "text" placeholder = "Message content" ref = {messageRef}
+                                   name = "message" className = "form-control" />
+                        </div>
+                        <div className = "form-group col-2">
+                            <div className = "form-check form-check-inline">
+                                <input className = "form-check-input" type = "radio"
+                                       checked = {messageType == 'public'}
+                                       onChange = {() => changeMessageType('public')}
+                                       name = "message_type" id = "public" value = "pubic" />
+                                <label className = "form-check-label" htmlFor = "public">Public</label>
+                            </div>
+                            <div className = "form-check form-check-inline">
+                                <input className = "form-check-input" type = "radio"
+                                       checked = {messageType == 'private'}
+                                       onChange = {() => changeMessageType('private')}
+                                       name = "message_type" id = "private" value = "private" />
+                                <label className = "form-check-label" htmlFor = "private">Private</label>
+                            </div>
+                        </div>
+                        <div className = "form-group col-2">
+                            <button className = "btn btn-block btn-primary" onClick = {() => publishEvent()}
+                                    type = "button">Publish Event
+                            </button>
+                        </div>
+                    </form>
+                </div>
+                <hr />
+                <div className = "col-12">
+                    <p className = "text-info">Subscribe for events</p>
+                    <form className = "form-row">
+                        <div className = "form-group col-9">
+                            <input type = "text" placeholder = "Topic to subscribe" className = "form-control" />
+                        </div>
+                        <div className = "form-group col-3">
+                            <button className = "btn btn-block btn-info" type = "button">Connect</button>
+                        </div>
+                    </form>
                 </div>
             </div>
+            <div className = "col-4">
+                <ul>
+                    {messages && messages.length > 0 ? messages.map((m, i) => {
+                        return <li className = {`text-${m.type}`} key = {i}>{m.text}</li>
+                    }) : null}
+                </ul>
+            </div>
         </div>
-    );
+
+    </div>);
 }
 
 export default App;
 
-if (document.getElementById('root')) {
+if ( document.getElementById('root') ) {
     ReactDOM.render(<App />, document.getElementById('root'));
 }
