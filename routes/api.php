197e52c053eba0043792ac940df125da736cb479
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-02-19 00:15:40 +0600

    broadcast message and service discovery added

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,30 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\Mercure\Publisher;
+use Symfony\Component\Mercure\Update;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post(
+    'broadcast',
+    function (Request $request, Publisher $publisher) {
+        $message = $request->get('message', 'empty message is received');
+        $topic = throw_unless($request->get('topic'), new Exception('Topic must be given'));
+        $isPrivate = $request->get('publish_type') === 'private';
+        $publisher(new Update($topic, $message, $isPrivate, null, 'name'));
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+        return response()->json(['error' => false, 'message' => 'Message published.'], 202);
+    }
+);
+
+Route::get(
+    'discovery',
+    function () {
+        $hubUrl = env('MERCURE_HUB_URL', 'http://127.0.0.1:9000');
+
+        return response()->json([])->withHeaders(
+            [
+                'link' => sprintf('<%s/.well-known/mercure>; rel="mercure"', $hubUrl),
+            ]
+        );
+    }
+);
--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Symfony\Component\Mercure\Publisher;
-use Symfony\Component\Mercure\Update;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get(
+    '/',
+    function () {
+        cookie()->queue(
+            'mercureAuthorization',
+            env('MERCURE_SUBSCRIBER_JWT_KEY'),
+            60,
+            '/.well-known/mercure',
+            null,
+            false,
+            false,
+            false,
+            'strict'
+        );
 
-Route::get('/', function () {
-    cookie()->queue(
-        'mercureAuthorization',
-        env('MERCURE_SUBSCRIBER_JWT_KEY'),
-        60,
-        '/.well-known/mercure',
-        null,
-        false,
-        false,
-        false,
-        'strict'
-    );
-    return view('main');
-});
-
-Route::get('data', function (Publisher $publisher) {
-    dd($publisher(new Update('http://example.com/books/1', json_encode(['value' => 'is given here']), true)));
-});
+        return view('main');
+    }
+);
